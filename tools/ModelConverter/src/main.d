86c95f93ca9908d0d7125cc56bb514ada2a8e089
Benjamin Thaut <code@benjamin-thaut.de>
2012-09-10 20:46:42 +0200

    working on model converted

--- tools/ModelConverter/src/main.d
+++ tools/ModelConverter/src/main.d
@@ -1,11 +1,89 @@
-
 import thBase.io;
+import thBase.string;
+import thBase.container.stack;
+import thBase.scoped;
+import thBase.file;
+import thBase.chunkfile;
+import assimp.assimp;
+
+void ProgressModel(string path)
+{
+  try
+  {
+		const(aiScene)* scene = Assimp.ImportFile(toCString(pFilename), 
+                                              aiPostProcessSteps.CalcTangentSpace |
+                                              aiPostProcessSteps.Triangulate |
+                                              aiPostProcessSteps.JoinIdenticalVertices |
+                                              aiPostProcessSteps.FlipUVs);// |
+    //aiPostProcessSteps.MakeLeftHanded); // |
+    //aiPostProcessSteps.PreTransformVertices );
+		if(scene is null){
+			throw New!FileException(format("Couldn't load model from file '%s'", pFilename[]));
+		}
+
+    rcstring outputFilename = path[0..$-3];
+    outputFilename ~= ".thModel";
+
+    auto outFile = scopedRef!Chunkfile(New!Chunkfile(ouputFilename, Chunkfile.Operation.Write));
+
+    outFile.startWriting("thModel", ModelFormatVersion.max);
+    scope(exit) outFile.endWriting();
+
+
+    //Materials
+    {
+      outFile.startWriteChunk("materials");
+      scope(exit) outFile.endWriteChunk();
+
+      if(scene.mMaterials !is null)
+      {
+        outFile.write!uint(scene.mNumMaterials);
+
+
+      }
+
+    }
+  }
+  catch(Exception ex)
+  {
+    writefln("Error progressing model '%s': %s", path, ex.toString()[]);
+    Delete(ex);
+  }
+}
 
 int main(string[] args)
 {
+  auto models = scopedRef!(Stack!string)(New!(Stack!string)());
   foreach(arg; args)
   {
-    if(
+    if(arg.endsWith(".dae", CaseSensitive.no))
+    {
+      if(thBase.file.exists(arg))
+        models.push(arg);
+      else
+      {
+        writefln("File: %s does not exist", arg);
+      }
+    }
   }
+  if(models.size == 0)
+  {
+    writefln("No model specified");
+    return 1;
+  }
+
+  try {
+    while(models.size > 0)
+    {
+      ProgressModel(models.pop());
+    }
+  }
+  catch(IThrowable ex)
+  {
+    writefln("Fatal error: %s", ex.toString()[]);
+    Delete(ex);
+    return -1;
+  }
+
   return 0;
 }
--- tools/ModelConverter/src/modeltypes.d
+++ tools/ModelConverter/src/modeltypes.d
@@ -1,5 +1,7 @@
 module modeltypes;
 
+import thBase.chunkfile;
+
 /**
 * exisiting texture types
 */
@@ -17,4 +19,33 @@ enum TextureType {
   SHININESS, /// shininess texture
   SPECULAR, /// specular texture
   UNKOWN /// unkown texture type
+}
+
+struct TextureInfo
+{
+  uint textureIndex;
+  rcstring textureName;
+
+  void ChunkfileSerialize(Chunkfile file)
+  {
+    if(file.operation == Chunkfile.Operation.Read)
+    {
+      file.write(textureIndex);
+      file.writeArray(textureName);
+    }
+    else if(file.operation == ChunkFile.Operation.Write)
+    {
+      file.read(textureIndex);
+      file.readAndAllocateArray(textureName);
+    }
+    else
+    {
+      assert(0, "Operation not implemented");
+    }
+  }
+}
+
+enum ModelFormatVersion : uint
+{
+  Version1 = 1 //Initial version
 }
